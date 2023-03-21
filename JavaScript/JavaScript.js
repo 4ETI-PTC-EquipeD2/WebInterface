e22cd24441bb471c3e13841010c7b87a9f698d7f
Oranium <gabriel.paulet-duprat@cpe.fr>
2023-03-21 15:22:35 +0100

    ui

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,3 +1,14 @@
+function enterCommand(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    //document.getElementById("commandLine").click();
+    console.log("Oui")
+  } 
+}
+
 function loader() {
     fetch("header.html")
         .then(response => {
@@ -14,3 +25,9 @@ function loader() {
     }
 
 loader()
+
+// Execute a function when the user presses a key on the keyboard
+var inputCommand = document.getElementsByClassName("command")[0];
+inputCommand.addEventListener("keypress", function(event) {
+    enterCommand(event);
+  }); 
