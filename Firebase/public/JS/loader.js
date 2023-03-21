8126a6df0cb37a0fee2c4a4f5dafa435deafa127
Mathis Gorvien <mathis.gorvien@gmail.com>
2023-03-21 17:24:25 +0100

    bug header

--- Firebase/public/JS/loader.js
+++ Firebase/public/JS/loader.js
@@ -1,37 +1,15 @@
 function loader() {
-    // if the page is not the index.html page :
-    console.log(location.pathname);
-    if (location.pathname != "/Firebase/public/index.html") {
-        fetch("header.html")
+    fetch("header.html")
         .then(response => {
             return response.text();
         })
         .then(data => {
             document.querySelector("header").innerHTML = data;
             liens = document.querySelectorAll("nav a");
-
             liens.forEach(a => {
-                console.log(a.href);
                 if (a.href == location.protocol + '//' + location.host + location.pathname)
                     a.classList.add('active');
             });
         });
     }
-    else {
-        fetch("HTML/header.html")
-        .then(response => {
-            return response.text();
-        })
-        .then(data => {
-            document.querySelector("header").innerHTML = data;
-            liens = document.querySelectorAll("nav a");
-
-            liens.forEach(a => {
-                console.log(a.href);
-                if (a.href == location.protocol + '//' + location.host + location.pathname)
-                    a.classList.add('active');
-            });
-        });
-    }
-}
 loader()
