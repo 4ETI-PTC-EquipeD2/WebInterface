78e7e81c591ff98425c37e9a1f8ccd7f339e07ea
Mathis Gorvien <mathis.gorvien@gmail.com>
2023-03-28 16:31:02 +0200

    firebase skeleton ok

---
+++ Firebase/public/JS/loader2.js
@@ -0,0 +1,19 @@
+function loader() {
+    fetch("header2.html")
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+        const liens = document.querySelectorAll("nav a");
+        liens.forEach(a => {
+        console.log(location.protocol + '//' + location.host + location.pathname);
+        console.log(a.href);
+        if (a.href == location.protocol + '//' + location.host + location.pathname) {
+            a.classList.add('active');
+        }
+        });
+    });
+}
+  
+document.addEventListener('DOMContentLoaded', loader);
