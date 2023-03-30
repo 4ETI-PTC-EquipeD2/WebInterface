1c0e7bfea81b0bbd3a0ebadd2136abe97bb1b7ea
Oranium <gabriel.paulet-duprat@cpe.fr>
2023-03-30 09:47:54 +0200

    Ajout des images de pokémon et des fonction avec

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -14,6 +14,39 @@ function enterCommand(event) {
   } 
 }
 
+async function loadJson(FichierName) { //Function to load JSON zarbi,métamorph,évolie,tortipouss,dracofeu,
+    let contenu_json = [];
+    await fetch(FichierName)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            contenu_json = json;
+        })
+    return contenu_json;
+}
+
+async function showPokemon(id){
+    contenu_json= await loadJson("../JSON/pokemon.json");
+    pokemon=contenu_json[id];
+    img_name=pokemon["image"];
+    img_name="../IMAGES/"+img_name;
+    drawImages("imPokemon",img_name);
+}
+
+function drawImages(id, img_name) {
+    let pokemon_img = new Image();
+    pokemon_img.onload = () => {
+        canvas = document.getElementById(id);
+        context = canvas.getContext('2d');
+        canvas.width = 800;
+        canvas.height = 1000;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(pokemon_img, 0, 0, canvas.width, canvas.height);
+    }
+    pokemon_img.src = img_name;
+}
+
 function loader() {
     fetch("header.html")
         .then(response => {
@@ -29,7 +62,11 @@ function loader() {
         });
     }
 
-loader()
+loader();
+//debugger
+let contenu_json=loadJson("../JSON/pokemon.json");
+console.log(contenu_json)
+showPokemon(0);
 
 // Execute a function when the user presses a key on the keyboard
 var divCommand = document.getElementsByClassName("command")[0];
