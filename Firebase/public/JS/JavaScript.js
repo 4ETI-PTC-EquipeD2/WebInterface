59ce122866850a82169a8ce2ddd4f0575a85c9eb
Mathis Gorvien <mathis.gorvien@gmail.com>
2023-04-04 17:14:46 +0200

    add prompt by Gabriel

---
+++ Firebase/public/JS/JavaScript.js
@@ -0,0 +1,5 @@
+import * as L from '../JS/Logs.js';
+import * as Pokemon from '../JS/Pokemon.js';
+let Logs = new L.logs();
+let tiplouf = new Pokemon.pokemon(Logs,0);
+tiplouf.showPokemon("imPokemon");
