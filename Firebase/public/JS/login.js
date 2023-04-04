a0f94d5f8a2394c7f8e6c2f2bc425c373ab3c396
Mathis Gorvien <mathis.gorvien@gmail.com>
2023-04-04 16:17:56 +0200

    clean up->index.html is a login page (index->home)

---
+++ Firebase/public/JS/login.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcvNxiU54UY-JRAKpn9e5AIEm5HuhaqSE",
+  authDomain: "projet-pokemon-9145b.firebaseapp.com",
+  databaseURL: "https://projet-pokemon-9145b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "projet-pokemon-9145b",
+  storageBucket: "projet-pokemon-9145b.appspot.com",
+  messagingSenderId: "350577809653",
+  appId: "1:350577809653:web:704206a9e3a2c7f4fe0dbc",
+  measurementId: "G-TLYG5H7X6Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+const submitButton = document.getElementById("submit");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const main = document.getElementById("main");
+
+const returnBtn = document.getElementById("return-btn");
+
+var email, password;
+
+submitButton.addEventListener("click", function() {
+  email = emailInput.value;
+  console.log(email);
+  password = passwordInput.value;
+  console.log(password);
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log("Success! Welcome back!");
+      window.alert("Success! Welcome back!");
+
+      // Redirect to the welcome page
+      window.location.href = "/Firebase/public/HTML/home.html";
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Error occurred. Try again.");
+      window.alert("Error occurred. Try again.");
+    });
+});
