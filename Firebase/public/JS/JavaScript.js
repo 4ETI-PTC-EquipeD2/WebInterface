0c4e16db8f34e0e8111644506095989f11c5c463
Oranium <gabriel.paulet-duprat@cpe.fr>
2023-04-18 15:12:08 +0200

    Bug enterCommand réglé

--- Firebase/public/JS/JavaScript.js
+++ Firebase/public/JS/JavaScript.js
@@ -1,5 +1,6 @@
 import * as L from '../JS/Logs.js';
 import * as Pokemon from '../JS/Pokemon.js';
 let Logs = new L.logs();
+Logs.defListener();
 let tiplouf = new Pokemon.pokemon(Logs,0);
 tiplouf.showPokemon("imPokemon");
--- Firebase/public/JS/Logs.js
+++ Firebase/public/JS/Logs.js
@@ -2,22 +2,24 @@ import * as p from './Prompt.js';
 export class logs extends p.Prompt {
     constructor(){
         super();
-        var divCommand = document.getElementsByClassName("command")[0];
-        divCommand.addEventListener("keypress", function(event) {
-            enterCommand(event);
-        }); 
     };
-    enterCommand(event) {
+    defListener(){
+        console.log(this,"thisd");
+        var divCommand = document.getElementsByClassName("command")[0];
+        divCommand.addEventListener("keypress", (event) => {
         // Function to enter command
         // If the user presses the "Enter" key on the keyboard
         if (event.key === "Enter") {
             // Cancel the default action, if needed
             let text="<br>"+document.getElementById("commandLine").value;
+            console.log(event.key,"Key");
             event.preventDefault();
-            write(text);
+            this.write(text);
             document.getElementById("commandLine").value = "";
-        console.log("Oui")
-      } 
+            console.log("Oui");
+    }
+})
+      
     }
     write(text) {
         var logs = document.getElementsByClassName("logs")[0].innerHTML;
