347d7cd453203bab7026532d934f26bbba10fc73
Oranium <gabriel.paulet-duprat@cpe.fr>
2023-04-20 11:22:39 +0200

    ajout des commandes basiques

--- Firebase/public/JS/Dresseur.js
+++ Firebase/public/JS/Dresseur.js
@@ -2,19 +2,26 @@ import * as poke from './Pokemon.js';
 //Class representing the robot. He can encounter pokemon and then realise actien on them.
 export class Dresseur{
     constructor(Logs){
-        this.hp=3
-        this.logs=Logs
-        this.encounter=null
+        this.hp=3;
+        this.logs=Logs;
+        this.encounter=null;
+        this.logs.addDresseur(this);
     }
     rencontre(id){
         this.encounter = new poke.pokemon(this.logs,id);
         this.encounter.showPokemon("imPokemon");
     }
     fuite(){
-        let text="<br>" + "Vous fuyez" + this.encounter.name;
+        let text="Vous fuyez " + this.encounter.name;
         this.logs.write(text);
         this.logs.clearImage("imPokemon");
         this.encounter=null;
+    }
+    attaque(){
+        let text="Vous attaquez " + this.encounter.name;
+        this.logs.write(text);
+        this.logs.clearImage("imPokemon")
+        //Enlever des hp
     }
     
 }
--- Firebase/public/JS/Logs.js
+++ Firebase/public/JS/Logs.js
@@ -2,28 +2,59 @@ import * as p from './Prompt.js';
 export class logs extends p.Prompt {
     constructor(){
         super();
+        this.dresseur=null;
     };
+    addDresseur(Dresseur){
+        this.dresseur=Dresseur;
+    }
     defListener(){
         console.log(this,"thisd");
         var divCommand = document.getElementsByClassName("command")[0];
         divCommand.addEventListener("keypress", (event) => {
         // Function to enter command
         // If the user presses the "Enter" key on the keyboard
-        if (event.key === "Enter") {
-            // Cancel the default action, if needed
-            let text="<br>"+document.getElementById("commandLine").value;
-            console.log(event.key,"Key");
-            event.preventDefault();
-            this.write(text);
-            document.getElementById("commandLine").value = "";
-            console.log("Oui");
+            if (event.key === "Enter") {
+                // Cancel the default action, if needed
+                let commande = document.getElementById("commandLine").value;
+                let text="-"+commande;
+                console.log(event.key,"Key");
+                event.preventDefault();
+                this.write(text);
+                this.analyseCommande(commande);
+                document.getElementById("commandLine").value = "";
+                console.log("Oui");
+            }
+        })
+   
     }
-})
-      
+    analyseCommande(commande){
+        commande=commande.split('_')
+        console.log(commande)
+        switch (commande[0]) {
+            case 'fuite':
+                this.dresseur.fuite();
+                break;
+            case 'attaque':
+                this.dresseur.attaque();
+                break;
+            case 'encounter':
+                let nb=parseInt(commande[1],10);
+                console.log(nb)
+                if (Object.is(nb,NaN)){
+                    this.write("Erreur sur le chiffre");
+                }
+                else{
+                    this.dresseur.rencontre(nb);
+                }
+                break
+            default:
+              this.write("La commande tapée n'existe pas.");
+          }
     }
     //Add text to the Logs
     write(text) {
         var logs = document.getElementsByClassName("logs")[0].innerHTML;
+        text='<br>'+text;
         logs+= text
         document.getElementsByClassName("logs")[0].innerHTML = logs;
     }
